5a7e23643a7a410a89e783a5489467ff52bcee0c
Romulo-ns <romulo.na.santos@gmail.com>
2026-07-08 13:12:19 +0100

    ex2

---
+++ M01/ex2/ft_plant_growth.py
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_plant_growth.py                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: romdo-na <romdo-na@student.42porto.com>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/07/08 12:07:07 by romdo-na          #+#    #+#              #
+#    Updated: 2026/07/08 13:12:03 by romdo-na         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class Plant:
+    name: str
+    height: float
+    age: int
+
+    def grow(self) -> None:
+        self.height += 0.8
+
+    def increase_age(self) -> None:
+        self.age += 1
+
+    def show(self) -> None:
+        print(f"{self.name}: {self.height:.1f}cm, {self.age} days old")
+
+if __name__ == "__main__":
+
+    rose = Plant()
+    rose.name = "Rose"
+    rose.height = 25
+    rose.age = 30
+    
+    print("=== Garden Plant Registry ===")
+    rose.show()
+
+    initial_height = rose.height
+
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        rose.increase_age()
+        rose.grow()
+        rose.show()
+
+    growth = rose.height - initial_height
+
+    print(f"Grpwth this week: {growth:.1f}cm")
