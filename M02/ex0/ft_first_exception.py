bf0d87d9d1e7146ff1f71ee0e9f1121d7f0db651
Romulo-ns <romulo.na.santos@gmail.com>
2026-08-01 10:52:40 +0100

    update

---
+++ M02/ex0/ft_first_exception.py
@@ -0,0 +1,19 @@
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+def test_temperature(temp: str) -> None:
+    try:
+        print(f"Temperature is now {input_temperature(temp)}°C\n")
+    except Exception as error:
+        print(f"Caught input_temperature error: {error}\n")
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===\n")
+
+    print("Input data is '25'")
+    test_temperature("25")
+
+    print("Input data is 'abc'")
+    test_temperature("abc")
+
+    print("All tests completed - program didn't crash!")
