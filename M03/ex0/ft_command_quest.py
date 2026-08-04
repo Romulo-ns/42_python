cc083105ba307f04dc1708d30ad7e3387a3efdb3
Romulo-ns <romulo.na.santos@gmail.com>
2026-08-04 12:34:45 +0100

    ex0 done

--- M03/ex0/ft_command_quest.py
+++ M03/ex0/ft_command_quest.py
@@ -8,13 +8,13 @@ def main() -> None:
     print(f"Program name: {sys.argv[0]}")
 
     argc = len(sys.argv)
-    count = 1
     if argc <= 1:
         print("No arguments provided!")
     else:
         print(f"Arguments received: {argc - 1}")
-        for i in sys.argv:
-            print(f"Argument {count}: {i}")
+        count = 1
+        while count < argc:
+            print(f"Argument {count}: {sys.argv[count]}")
             count += 1
     print(f"Total arguments: {argc}")
 
