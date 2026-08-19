9cfd707f08ac5d147c8cfd8accc1be700d9910b0
Romulo-ns <romulo.na.santos@gmail.com>
2026-08-19 16:57:07 +0100

    exam

---
+++ M03/ex2/ft_coordinate_system.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.10
+
+import sys
+
+def get_player_pos() -> None:
+    print("=== Game Coordinate System ===\n")
+    print("Get a first set of coordinates")
+
+
+
+
+
+
+
+
+
+
+
+
+def main() -> None:
+    get_player_pos()
+
+
+if __name__ == "__main__":
+    main()
